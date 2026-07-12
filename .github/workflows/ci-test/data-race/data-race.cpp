b9bd701050eeab7007aaf47b5a283aba07b4704c
John McFarlane <git@john.mcfarlane.name>
2026-07-12 22:20:21 +0100

    Fix intermitend CI test failure
    - in TSan test
      - caused by failure to generate race condition
    - by pausing at start of thread.

--- .github/workflows/ci-test/data-race/data-race.cpp
+++ .github/workflows/ci-test/data-race/data-race.cpp
@@ -1,8 +1,15 @@
+#include <chrono>
 #include <thread>
 
 auto main() -> int
 {
     auto shared_state{0};
-    std::jthread const thread{[&] { shared_state = 1; }};
+    std::jthread const thread{[&] {
+        using namespace std::chrono_literals;
+        constexpr auto short_time{10ms};
+        std::this_thread::sleep_for(short_time);
+
+        shared_state = 1;
+    }};
     return shared_state;
 }
